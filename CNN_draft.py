9aa797e5731f668ea200321bf2b8a3d77ea3fa3e
rendldav <116627323+rendldav@users.noreply.github.com>
2023-12-02 15:21:09 +0100

    Update CNN_draft.py
    Modified CNN, uses parquet files or numpy files, logging added, testing added, better dataset sizes calculation

--- CNN_draft.py
+++ CNN_draft.py
@@ -10,6 +10,45 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+import wandb
+from pytorch_lightning.loggers import WandbLogger
+from processing import ParquetProcess
+
+
+class ParquetFolderDataset(Dataset):
+    def __init__(self, root_dir, landmarks, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.landmarks=landmarks
+        self.parquet_files = []
+        self.labels = []
+        self.class_to_idx = {}
+
+        for idx, class_name in enumerate(sorted(os.listdir(root_dir))):
+            class_dir = os.path.join(root_dir, class_name)
+            if os.path.isdir(class_dir):
+                self.class_to_idx[class_name] = idx
+                for file in os.listdir(class_dir):
+                    if file.endswith('.parquet'):
+                        self.parquet_files.append(os.path.join(class_dir, file))
+                        self.labels.append(idx)
+
+    def __len__(self):
+        return len(self.parquet_files)
+
+    def __getitem__(self, idx):
+        parquet_path = self.parquet_files[idx]
+        label = self.labels[idx]
+        # Load .npy file
+        readed_data = ParquetProcess(parquet_path, self.landmarks, 140)
+        sample = readed_data.tensor
+        sample = sample.astype(np.float32)
+        sample = np.expand_dims(sample, axis=0)
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample, label
+
 
 class NpyFolderDataset(Dataset):
     def __init__(self, root_dir, transform=None):
@@ -51,7 +90,7 @@ class NpyFolderDataset(Dataset):
 
 
 class AslDataModule(pl.LightningDataModule):
-    def __init__(self, data_dir=r"E:\asl-signs\tensors", batch_size: int = 32, val_split: float = 0.2):
+    def __init__(self, landmarks_idx, data_dir=r"E:\asl-signs\parquets_sorted", batch_size: int = 32, val_split: float = 0.2):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
@@ -59,25 +98,28 @@ class AslDataModule(pl.LightningDataModule):
         self.dims = (420, 70, 70)
         self.num_classes = 250
         self.val_split = val_split
-
-    #def prepare_data(self):
-    # tady potenciálně parquet_proccess
+        self.landmark = landmarks_idx
 
     def setup(self, stage: str=None):
         if stage == 'fit' or stage is None:
-            full_dataset = NpyFolderDataset(root_dir=self.data_dir)
+            full_dataset = ParquetFolderDataset(root_dir=self.data_dir, landmarks=self.landmark)
             print(f"Full dataset size: {len(full_dataset)}")
-            val_size = int(len(full_dataset))
-            train_size = len(full_dataset) - val_size
-            self.train_dataset, self.val_dataset = random_split(full_dataset, [2000, len(full_dataset)-2000])
+            val_size = int(len(full_dataset) * 0.2)
+            train_size = int(len(full_dataset) * 0.7)
+            test_size = len(full_dataset) - train_size - val_size
+            self.train_dataset, self.val_dataset, self.test_dataset = random_split(full_dataset, [train_size, val_size, test_size])
             print(f"Train dataset size: {len(self.train_dataset)}")  # Debug print
             print(f"Validation dataset size: {len(self.val_dataset)}")  # Debug print
+            print(f"Test dataset size: {len(self.test_dataset)}")  # Debug print
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, num_workers=4)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=4)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.batch_size)
 
 class AslLitModel(pl.LightningModule):
     def __init__(self, input_shape, num_classes, learning_rate=3e-4):
@@ -167,12 +209,22 @@ class AslLitModel(pl.LightningModule):
 
 
 def main():
-    path = r"E:\asl-signs\tensors"
-    dm = AslDataModule()
+    selected_landmark_indices = [33, 133, 159, 263, 46, 70, 4, 454, 234, 10, 338, 297, 332, 61, 291, 0, 78, 14, 317,
+                                 152, 155, 337, 299, 333, 69, 104, 68, 398]
+    wandb.login(key='6b4346cf1caf31e2c470b0f4b7e338da7bf74825')
+    path = r"E:\asl-signs\parquets_sorted"
+    dm = AslDataModule(selected_landmark_indices, path)
     dm.setup()
     model = AslLitModel((1, 70, 70, 420), dm.num_classes)  # Make sure the input shape matches your data
-    trainer = pl.Trainer(max_epochs=2)
+    wandb_logger = WandbLogger(project='ASL', job_type='train')
+    checkpoint_callback = pl.callbacks.ModelCheckpoint()
+    trainer = pl.Trainer(logger=wandb_logger, max_epochs=2,
+                         callbacks=[checkpoint_callback]
+                         )
+
     trainer.fit(model, dm)
+    trainer.test(dataloaders=dm.test_dataloader())
+    wandb.finish()
 
 
 if __name__ == '__main__':

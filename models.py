94b4bd53b89d13dd5267af75afb9c6857aa5646e
rendldav <116627323+rendldav@users.noreply.github.com>
2023-12-08 15:36:45 +0100

    Second model added

--- models.py
+++ models.py
@@ -90,3 +90,84 @@ class AslLitModel(pl.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
         return optimizer
 
+
+class AsLModel2(pl.LightningModule):
+    def __init__(self, input_shape, num_classes, learning_rate=3e-4):
+        super().__init__()
+        self.save_hyperparameters()
+        self.learning_rate = learning_rate
+        self.num_classes = num_classes
+
+        self.conv1 = nn.Conv3d(1, 32, (3, 3, 3))
+        nn.init.constant_(self.conv1.bias, 0.01)
+        self.conv2 = nn.Conv3d(32, 32, (3, 3, 3))
+        nn.init.constant_(self.conv2.bias, 0.01)
+        self.conv3 = nn.Conv3d(32, 64, (3, 3, 3))
+        self.conv4 = nn.Conv3d(64, 64, (2, 2, 2))
+
+        self.pool = nn.MaxPool3d((2, 2, 2))
+        self.dropout1 = nn.Dropout(0.6)
+        self.dropout2 = nn.Dropout(0.7)
+        self.dropout3 = nn.Dropout(0.5)
+
+        n_sizes = self._get_conv_output(input_shape)
+
+        self.fc1 = nn.Linear(n_sizes, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, num_classes)
+
+        self.accuracy = Accuracy(task="multiclass", num_classes=self.num_classes)
+
+    def _get_conv_output(self, shape):
+        with torch.no_grad():
+            input = torch.autograd.Variable(torch.rand(1, *shape))
+            output = self._feature_extractor(input)
+            return int(numpy.prod(output.size()))
+
+    def _feature_extractor(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+        return x
+
+    def forward(self, x):
+        x = self._feature_extractor(x)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.dropout1(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout2(x)
+        x = self.fc3(x)
+        return F.log_softmax(x, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss)
+        self.log('train_acc', self.accuracy(y_hat, y), prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('val_loss', loss, prog_bar=True)
+        self.log('val_acc', self.accuracy(y_hat, y), prog_bar=True)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('test_loss', loss, prog_bar=True)
+        self.log('test_acc', self.accuracy(y_hat, y), prog_bar=True)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
